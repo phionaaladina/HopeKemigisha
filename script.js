b94ae43b7500ee073bef70204b3e0210557ae692
Phiona Aladina <phionaaladina@gmail.com>
2024-03-31 19:16:33 +0300

    form data collection using SheetDB

--- script.js
+++ script.js
@@ -4,3 +4,89 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+
+
+var form = document.getElementById('sheetdb-form');
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          fetch(form.action, {
+              method : "POST",
+              body: new FormData(document.getElementById("sheetdb-form")),
+          }).then(
+              response => response.json()
+          ).then((html) => {
+            // you can put any JS code here
+            window.open('message.html', '_blank');
+
+          });
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("contactForm").addEventListener("submit", function (e) {
+//   e.preventDefault(); // Prevent the default form submission
+
+//   // Display a message indicating form submission is in progress
+//   document.getElementById("message").textContent = "Submitting..";
+//   document.getElementById("message").style.display = "block";
+//   document.getElementById("submitButton").disabled = true;
+
+//   // Collect the form data
+//   var formData = new FormData(this);
+
+//   // Send a POST request to the Google Apps Script web app
+//   fetch("https://script.google.com/macros/s/AKfycbypeptCscfO3XMEpNbvg1KgT1HPqRSbdcVf-03zaq9FIEAzQAzK4PsR7PRGCv2FXTFI/exec", {
+//     method: "POST",
+//     body: formData
+//   })
+//     .then(function (response) {
+//       // Check if the request was successful
+//       if (!response.ok) {
+//         throw new Error("Failed to submit the form.");
+//       }
+//       return response.text(); // Assuming your script returns plain text response
+//     })
+//     .then(function (data) {
+//       // Display a success message
+//       document.getElementById("message").textContent = "Data submitted successfully!";
+//       document.getElementById("message").style.backgroundColor = "green";
+//       document.getElementById("message").style.color = "white";
+//       document.getElementById("contactForm").reset(); // Reset the form
+//     })
+//     .catch(function (error) {
+//       // Handle errors and display an error message
+//       console.error(error);
+//       document.getElementById("message").textContent = "An error occurred while submitting the form.";
+//       document.getElementById("message").style.backgroundColor = "red";
+//     })
+//     .finally(function () {
+//       // Enable the submit button and hide the message after a delay
+//       document.getElementById("submitButton").disabled = false;
+//       setTimeout(function () {
+//         document.getElementById("message").textContent = "";
+//         document.getElementById("message").style.display = "none";
+//       }, 3000); // 3 seconds delay
+//     });
+// });
